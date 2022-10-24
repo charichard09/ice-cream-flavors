d7ccc793a806b407e43e82dc77880ab45ee80ce6
Richard <charichard09@gmail.com>
2022-10-24 15:38:00 -0700

    add transportation exercise
    Co-authored-by: Drew White <d.white0002@gmail.com>

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,6 +1,22 @@
 function handleForm(event) {
   event.preventDefault();
+  // get array of all checked inputs and assign to variable
   const userSelections = document.querySelectorAll("input[name=transportation-option]:checked");
+  // convert userSelections from Nodelist object to and array with Array.from(array)
+  const userSelectionsArray = Array.from(userSelections);
+
+  // create results heading
+  const resultHeading = document.createElement("h2");
+  resultHeading.append("You use the following methods of transportation to travel to work or school:");
+  document.body.append(resultHeading);
+
+  // loop through userSelectionArray. For each element, create a paragraph element, append element.value 
+  // inside paragraph object, then append paragraph object to body object 
+  userSelectionsArray.forEach(function (element) {
+    const paragraph = document.createElement("p");
+    paragraph.append(element.value);
+    document.body.append(paragraph);
+  })
 }
 
 window.addEventListener("load", function() {
